ece8d371c05ae68196d524a04b2dd245c42bff46
sagar <sagardanuwar30@gmail.com>
2025-09-06 14:00:53 +0545

    sagar first file

---
+++ week1/sagar/task.java
@@ -0,0 +1,100 @@
+public class task {
+    // public class Main {
+//     public static void main(String[] args) {
+//         System.out.print("Hello, World!\n");
+//         System.out.print("This is my first program\n");
+//         System.out.print("I am on module ST4003CEM\n");
+//     }
+    
+    
+// }
+
+
+// public class Main{
+//     public static void main(String[] args) {
+//         System.out.println("A \"quoted\" String is");
+//         System.out.println("'much' better if you learn");
+//         System.out.println("the rules of \"escape sequences.\"");
+//         System.out.println("Also, \"\" represents an empty");
+//         System.out.println("String. Don't forget: use \\\"");
+//         System.out.println("instead of \"!");
+//         System.out.println("\"is not the same as \"");
+
+//     }
+// }
+
+
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("*");
+//         System.out.println("**");
+//         System.out.println("***");
+//         System.out.println("****");
+//         System.out.println("*****");
+//     }
+// }
+
+// public class Main {
+//     public static void main(String[] args) {
+//         System.out.println("*********");
+//         System.out.println("*********");
+//         System.out.println("*********");
+//         System.out.println("*********");
+//         System.out.println("*********");
+//     }
+// }
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("A well-formed Java program has a main method with { and } braces.");
+        System.out.println("A System.out.println statement has ( and ) and usually a String that starts and");
+        System.out.println("ends with a \" character.");
+        System.out.println("(But we type \\\" instead!)");
+        System.out.println("What is the difference between");
+        System.out.println("a ' and a \"?  Or between a \" and a \\\"?");
+        System.out.println("One is what we see when we're typing our program. The other is what appears on the");
+        System.out.println("\"console\"");
+
+    
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* to print two number number */
+
+
+
+
+
+/*  public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        Integer a=10;
+        Integer b=20;
+        System.out.println(a+b);
+    }
+    
+    
+}/* to print two number number */
+    
+}
